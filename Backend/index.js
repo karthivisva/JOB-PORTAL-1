671c22cf9f59431649918bd9eef3e98fd28dbd98
Ankit Pathak <pathakankit688@gmail.com>
2024-12-16 22:35:46 +0530

    companycompanyRoute);companyRoute);

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
 dotenv.config({});
 const app = express();
 
@@ -24,8 +25,7 @@ const PORT = process.env.PORT || 5001;
 //api's
 
 app.use("/api/user", userRoute);
- 
-
+app.use("/api/company", companyRoute);
 
 app.listen(PORT, () => {
   connectDB();
