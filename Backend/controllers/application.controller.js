5a4bb6d052e88873b90473961bcf9dc1a95f0e63
Ankit Pathak <pathakankit688@gmail.com>
2024-12-17 22:43:30 +0530

    application controller

--- Backend/controllers/application.controller.js
+++ Backend/controllers/application.controller.js
@@ -66,18 +66,48 @@ export const getAppliedJobs = async (req, res) => {
   }
 };
 
-
 export const getApplicants = async (req, res) => {
-    try {
-        const jobId = req.params.id;
-       const job = await Job.findById(jobId).populate({
-        path: "applications",
-        options: { sort: { createdAt: -1 } },
-        populate: { path: "applicant", options: { sort: { createdAt: -1 } } },
-       });
-        
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Server error", success: false });        
+  try {
+    const jobId = req.params.id;
+    const job = await Job.findById(jobId).populate({
+      path: "applications",
+      options: { sort: { createdAt: -1 } },
+      populate: { path: "applicant", options: { sort: { createdAt: -1 } } },
+    });
+    if (!job) {
+      return res.status(404).json({ message: "Job not found", success: false });
     }
-}
+
+    return res.status(200).json({ job, success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error", success: false });
+  }
+};
+
+export const updateStatus = async (req, res) => {
+  try {
+    const { status } = req.params;
+    const applicationId = req.params.id;
+    if (!status) {
+      return res
+        .status(400)
+        .json({ message: "Invalid status", success: false });
+    }
+
+    //find the application by applicant id
+    const application = await Application.findById({ _id: applicationId });
+    if (!application) {
+      return res
+        .status(404)
+        .json({ message: "Application not found", success: false });
+    }
+
+    //update the status
+    application.status = status.toLowerCase();
+    await application.save();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error", success: false });
+  }
+};
