6ce0d913edf7ff7f56e4f92cad20baa879cdf81f
Ankit Pathak <pathakankit688@gmail.com>
2024-12-17 22:36:57 +0530

    applicantion controller

---
+++ Backend/controllers/application.controller.js
@@ -0,0 +1,83 @@
+import { Application } from "../models/application.model.js";
+
+export const applyJob = async (req, res) => {
+  try {
+    const userId = req.id;
+    const jobId = req.params.id;
+    if (!jobId) {
+      return res
+        .status(400)
+        .json({ message: "Invalid job id", success: false });
+    }
+    // check if the user already has applied for this job
+    const existingApplication = await Application.findOne({
+      job: jobId,
+      applicant: userId,
+    });
+    if (existingApplication) {
+      return res.status(400).json({
+        message: "You have already applied for this job",
+        success: false,
+      });
+    }
+    //check if the job exists or not
+    const job = await Job.findById(jobId);
+    if (!job) {
+      return res.status(404).json({ message: "Job not found", success: false });
+    }
+    // create a new application
+
+    const application = new Application({
+      job: jobId,
+      applicant: userId,
+    });
+    job.applications.push(newApplication._id);
+    await job.save();
+
+    return res
+      .status(201)
+      .json({ message: "Application submitted", success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error", success: false });
+  }
+};
+
+export const getAppliedJobs = async (req, res) => {
+  try {
+    const userId = req.id;
+    const applications = await Application.find({ applicant: userId })
+      .sort({ createdAt: -1 })
+      .populate({
+        path: "job",
+        options: { sort: { createdAt: -1 } },
+        populate: { path: "company", options: { sort: { createdAt: -1 } } },
+      });
+    if (!application) {
+      return res
+        .status(404)
+        .json({ message: "No applications found", success: false });
+    }
+
+    return res.status(200).json({ applications, success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error", success: false });
+  }
+};
+
+
+export const getApplicants = async (req, res) => {
+    try {
+        const jobId = req.params.id;
+       const job = await Job.findById(jobId).populate({
+        path: "applications",
+        options: { sort: { createdAt: -1 } },
+        populate: { path: "applicant", options: { sort: { createdAt: -1 } } },
+       });
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error", success: false });        
+    }
+}
