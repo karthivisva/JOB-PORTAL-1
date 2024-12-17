aea2ced63f88f08f690db2388ae2aa868624f5af
Ankit Pathak <pathakankit688@gmail.com>
2024-12-17 22:49:03 +0530

    applicationRoute

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,8 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+
 dotenv.config({});
 const app = express();
 
@@ -28,6 +30,7 @@ const PORT = process.env.PORT || 5001;
 app.use("/api/user", userRoute);
 app.use("/api/company", companyRoute);
 app.use("/api/job", jobRoute);
+app.use("/api/application", applicationRoute);
 
 app.listen(PORT, () => {
   connectDB();
