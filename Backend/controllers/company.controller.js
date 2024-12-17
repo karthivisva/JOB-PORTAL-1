dc213db9364761cc64f07e0dd8b4845f8c43f44b
Ankit Pathak <pathakankit688@gmail.com>
2024-12-17 10:28:47 +0530

    company controller

--- Backend/controllers/company.controller.js
+++ Backend/controllers/company.controller.js
@@ -1,28 +1,37 @@
 import { Company } from "../models/company.model.js";
+
 export const registerCompany = async (req, res) => {
   try {
     const { companyName } = req.body;
     if (!companyName) {
-      return res.status(400).json({ message: "Company name is required" });
+      return res.status(401).json({
+        message: "Company name is required",
+        success: false
+      });
     }
     let company = await Company.findOne({ name: companyName });
     if (company) {
-      return res.status(400).json({ message: "Company already exists" });
-    }
-
+      return res.status(401).json({
+        message: "Company already exists",
+        success: false
+      });
+    };
     company = await Company.create({
       name: companyName,
-      userId: req.id,
+      userId: req.id
     });
     return res.status(201).json({
-      message: "Company created successfully",
+      message: "Company registered successfully.",
       company,
-      success: true,
-    });
+      success: true
+    })
   } catch (error) {
-    console.error(error);
+    console.log(error);
   }
-};
+}
+
+
+
 
 export const getAllCompanies = async (req, res) => {
   try {
