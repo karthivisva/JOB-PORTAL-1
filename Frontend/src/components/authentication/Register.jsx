d09654378dbdaddde965b8aaa7aa7372f2a4c770
Ankit Pathak <pathakankit688@gmail.com>
2024-12-20 02:27:34 +0530

    register complete

--- Frontend/src/components/authentication/Register.jsx
+++ Frontend/src/components/authentication/Register.jsx
@@ -3,7 +3,10 @@ import Navbar from "../components_lite/Navbar";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { RadioGroup } from "../ui/radio-group";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { USER_API_ENDPOINT } from "@/utils/data";
+import { toast } from "sonner";
 
 const Register = () => {
   const [input, setInput] = useState({
@@ -15,6 +18,9 @@ const Register = () => {
     file: "",
   });
 
+
+const navigate = useNavigate();
+
   const changeEventHandler = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
   };
@@ -24,7 +30,30 @@ const Register = () => {
 
   const submitHandler = async (e) => {
     e.preventDefault();
-    console.log(input);
+    const formData = new FormData();
+    formData.append("fullname", input.fullname);
+    formData.append("email", input.email);
+    formData.append("password", input.password);
+    formData.append("role", input.role);
+    formData.append("phoneNumber", input.phoneNumber);
+    if (input.file) {
+      formData.append("file", input.file);
+    }
+    try {
+      const res = await axios.post(`${USER_API_ENDPOINT}/register`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        withCredentials: true,
+      });
+      if(res.data.success){
+        navigate("/login");
+        toast.success(res.data.message);
+         
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div>
