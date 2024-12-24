e9ba034cadd94ea4c8b5b819732598d3b1903fd3
Ankit Pathak <pathakankit688@gmail.com>
2024-12-24 21:44:46 +0530

    browse

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Home from "./components/components_lite/Home";
 import PrivacyPolicy from "./components/components_lite/PrivacyPolicy.jsx";
 import TermsofService from "./components/components_lite/TermsofService.jsx";
 import Jobs from "./components/components_lite/Jobs.jsx";
+import Browse from "./components/components_lite/Browse.jsx";
 
 const appRouter = createBrowserRouter([
   { path: "/", element: <Home /> },
@@ -33,6 +34,9 @@ const appRouter = createBrowserRouter([
   {
     path:"/Home",
     element: <Home />,
+  },{
+    path: "/Browse",
+    element: <Browse />,
   }
 ]);
 
