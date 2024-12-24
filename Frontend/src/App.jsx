148867b2a3d6b97f9667e60b2a9a1e6431d5224d
Ankit Pathak <pathakankit688@gmail.com>
2024-12-24 20:08:19 +0530

    Navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/components_lite/Home";
 import PrivacyPolicy from "./components/components_lite/PrivacyPolicy.jsx";
 import TermsofService from "./components/components_lite/TermsofService.jsx";
+import Jobs from "./components/components_lite/Jobs.jsx";
 
 const appRouter = createBrowserRouter([
   { path: "/", element: <Home /> },
@@ -25,6 +26,14 @@ const appRouter = createBrowserRouter([
     path: "/TermsofService",
     element: <TermsofService />,
   },
+  {
+    path: "/Jobs",
+    element: <Jobs />,
+  },
+  {
+    path:"/Home",
+    element: <Home />,
+  }
 ]);
 
 function App() {
